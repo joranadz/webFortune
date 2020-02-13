c6dc93b632cd2e0509449a537c8672cb87b44629
JoanaRadze <joana.radzeviciute@gmail.com>
2020-02-13 23:23:40 +0200

    slides and admin

--- weShop/settings.py
+++ weShop/settings.py
@@ -30,15 +30,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fortune.apps.FortuneConfig'
-]
+    'fortune.apps.FortuneConfig',
+    'django_filters'
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
